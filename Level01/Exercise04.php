1c92b600ba39c8fa62bb12b1775352bb96bfd47f
Tamara <tamara_tmb@hotmail.com>
2026-09-22 10:24:03 +0200

    feat: conclusion exercise 04

---
+++ Level01/Exercise04.php
@@ -0,0 +1,24 @@
+<?php
+
+//Haz un array asociativo que represente información de ti mismo/a. 
+//En concreto debe incluir:
+
+// nombre
+// edad
+// email
+// comida favorita
+
+
+$dados = array (
+    "nombre" => "Tamara",
+    "edad" => "34",
+    "email" => "tamara_tmb@hotmail.com",
+    "comida favorita" => "Pizza"
+);
+
+echo "nombre:" . $dados["nombre"] . PHP_EOL;
+    echo "edad:" . $dados["edad"] . PHP_EOL;
+    echo "email:" . $dados["email"] . PHP_EOL;
+    echo "comida favorita:" . $dados["comida favorita"] . PHP_EOL;
+;
+
